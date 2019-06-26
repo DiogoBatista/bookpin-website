833c8878fa666e6d5e12992a0d6d1d227eae0f61
diogo batista <diogo@themainingredient.co>
2019-06-26 21:59:07 +0200

    open new tab on click

--- src/components/GooglePlayButton/GooglePlayButton.tsx
+++ src/components/GooglePlayButton/GooglePlayButton.tsx
@@ -15,7 +15,7 @@ export const GooglePlayButton = ({ bookName, bookId }: GooglePlayButtonProps) =>
         (bookName && bookId) ? (
           <a onClick={() => {
             Event({ category: 'GooglePlayButton', action: 'click_to_buy', label: "book_page" })
-          }} href={`https://play.google.com/store/books/details/${bookName}?id=${bookId}&PAffiliateId=${AFFILIATE_ID}`}>
+          }} target="_blank" href={`https://play.google.com/store/books/details/${bookName}?id=${bookId}&PAffiliateId=${AFFILIATE_ID}`}>
             <img className="google-play-button" src="https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png" alt="Get it on Google Play" />
           </a>
         ) : ''
